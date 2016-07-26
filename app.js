435d03afb5585346269cf942b04252e08528b65b
icue <icue126@gmail.com>
2016-07-26 11:17:46 -0700

    added mongoose and others

--- app.js
+++ app.js
@@ -4,11 +4,17 @@ var favicon = require('serve-favicon');
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
+var multer = require('multer');
+var mongoose = require('mongoose');
+var session = require('express-session');
 
 var routes = require('./routes/index');
 var users = require('./routes/users');
 
 var app = express();
+
+global.db_handle = require('./database/db_handle');
+global.db = mongoose.connect("mongodb://localhost:27017/nodedb");
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
@@ -23,12 +29,20 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
+app.use(multer());
 
 app.use('/', routes);
 app.use('/users', users);
 app.use('login',routes);
 app.use('/home',routes);
 app.use('/logout',routes);
+
+app.use(session({ 
+    secret: 'secret',
+    cookie:{ 
+        maxAge: 1000*60*60;  //1 hour
+    }
+}));
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
@@ -61,5 +75,15 @@ app.use(function(err, req, res, next) {
   });
 });
 
+app.use(function(req, res, next){ 
+    res.locals.user = req.session.user;   // get user from session
+    var err = req.session.error;
+    delete req.session.error;
+    res.locals.message = "";
+    if(err){ 
+        res.locals.message = '<div class="alert alert-danger" style="margin-bottom:15px; color:red;">'+err+'</div>';
+    }
+    next();
+});
 
 module.exports = app;
