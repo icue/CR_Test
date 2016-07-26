c6e7680138924c9e03fdb7cac9f61fb24d26b2d9
icue <icue126@gmail.com>
2016-07-26 10:49:34 -0700

    started working on view

--- app.js
+++ app.js
@@ -26,6 +26,9 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', routes);
 app.use('/users', users);
+app.use('login',routes);
+app.use('/home',routes);
+app.use('/logout',routes);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
