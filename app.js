5018414dba03cc02ed745d65b936b0070af7f9c2
icue <icue126@gmail.com>
2016-08-01 09:49:23 -0700

    removed multer and favicon

--- app.js
+++ app.js
@@ -1,13 +1,10 @@
 var express = require('express');
 var path = require('path');
-var favicon = require('serve-favicon');
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
-var multer = require('multer');
 var mongoose = require('mongoose');
 var session = require('express-session');
-var upload = multer({ dest: './uploads' });
 var routes = require('./routes/index');
 var users = require('./routes/users');
 var app = express();
@@ -20,14 +17,11 @@ app.set('views', path.join(__dirname, 'views'));
 app.engine("html",require("ejs").renderFile); //help ejs recognize .html files
 app.set('view engine', 'html');
 
-// uncomment after placing your favicon in /public
-//app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
 app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
-//app.use(multer());
 
 app.use(session({
     resave: false,
